087e0eb787464581addadda2ccbd8c8d6b74e456
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-07-03 00:14:47 +0300

    ...

---
+++ Foody.EntityLayer/Concrete/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.EntityLayer.Concrete
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerTitle { get; set; }
+        public string CustomerImage { get; set; }
+        public string CustomerComment { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
