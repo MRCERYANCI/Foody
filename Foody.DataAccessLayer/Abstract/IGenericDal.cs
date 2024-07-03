4b92dc85d5ebbb16b24b3451b4dc09c807c9e3c5
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-07-03 17:02:56 +0300

    ...

---
+++ Foody.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        Task Insert(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+    }
+}
