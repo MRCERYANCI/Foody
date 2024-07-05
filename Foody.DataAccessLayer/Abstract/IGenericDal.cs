d5d55d06e5984511e5626eaecd2f40871f3b4cd0
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-07-05 10:33:16 +0300

    ...

--- Foody.DataAccessLayer/Abstract/IGenericDal.cs
+++ Foody.DataAccessLayer/Abstract/IGenericDal.cs
@@ -10,7 +10,7 @@ namespace Foody.DataAccessLayer.Abstract
     {
         Task Insert(T entity);
         Task Update(T entity);
-        Task Delete(T entity);
+        Task Delete(int id);
         Task<List<T>> GetAll();
         Task<T> GetById(int id);
     }
