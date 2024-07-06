ae0210aaf2c7f4c7287c97ec90af925319c5e819
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-07-06 11:01:56 +0300

    ...

---
+++ Foody.DataAccessLayer/Concrete/FoodyContext.cs
@@ -0,0 +1,27 @@
+﻿using Foody.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.Concrete
+{
+    public class FoodyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=192.168.1.56; Database=SignalRrestoDb; Integrated Security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+    }
+}
