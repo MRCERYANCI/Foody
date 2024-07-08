44b7d0151e0067cdc65692d63510ccb0b1f0a734
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-07-08 13:35:36 +0300

    ...

---
+++ Foody.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Foody.DataAccessLayer.Abstract;
+using Foody.DataAccessLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly FoodyContext _context;
+
+        public GenericRepository(FoodyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Delete(int id)
+        {
+            var values = await GetById(id);
+            _context.Set<T>().Remove(values);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
